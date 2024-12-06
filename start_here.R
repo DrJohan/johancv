a9158407e74bc01e282a3c5fb2e141bbcfa76b0c
DrJohan <johan@ms.rcsiucd.edu.my>
2024-12-06 19:06:31 +0800

    First commit

---
+++ start_here.R
@@ -0,0 +1,8 @@
+library(datadrivencv)
+library(here)
+datadrivencv::use_datadriven_cv(
+  full_name = "Dr Johan Ibrahim MD",
+  data_location = "https://docs.google.com/spreadsheets/d/1_Y3HPpenl9Q0rovGZUmto6kxSCrtv8BsTGSfdk-xdXk/edit?usp=sharing",
+  html_location =  "https://drjohan.github.io/johancv/index.html",
+  pdf_location = ""
+)
